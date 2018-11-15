7beb1d11c9af843b472dc259ce9fef09d9751e6c
ioannidisa <42744494+ioannidisa@users.noreply.github.com>
2018-11-15 16:56:51 -0500

    Update forensicsScript.ps1

--- forensicsScript.ps1
+++ forensicsScript.ps1
@@ -40,29 +40,28 @@ Write-Output "Beginning YARA analysis..."
 $yarasection="`nYARA: "
 $yarasection | Out-File "$path\$text" -Append
 
-# User specifies the directory they want??
-#$yarachoice=Read-Host -Prompt "Do you want to specify a specific directory for Yara to search through? (Y for Yes, N for No): "
-#$pathchildCount=0
-#if($yarachoice -eq 'y' -OR $yarachoice -eq 'Y'){
-#   $yarapath=Read-Host -Prompt "Enter the Full Path of the directory you want to search: "
-#   Get-ChildItem -Recurse -filter *.exe $yarapath 2> $null |
-#   ForEach-Object { Write-Host -foregroundcolor "green" "Scanning"$_.FullName $_.Name; $pathchildCount+=1; ./yara64.exe -d filename=$_.Name TOOLKIT.yar $_.FullName 2> $path\$text }
-    #$yarapathCount="Number of files scanned for " + $yarapath + " directory: " + $pathchildCount.Count
-    #$yarapathCount | Out-File "$path\$text" -Append
-#}
+# User specifies the directory they want
+$yarachoice=Read-Host -Prompt "Do you want to specify a specific directory for Yara to search through? (Y for Yes, N for No) "
+$pathchildCount=0
+if($yarachoice -eq 'y' -OR $yarachoice -eq 'Y'){
+   $yarapath=Read-Host -Prompt "Enter the Full Path of the directory you want to search "
+   Get-ChildItem -Recurse -filter *.exe $yarapath 2> $null |
+   ForEach-Object { Write-Host -foregroundcolor "green" "Scanning"$_.FullName $_.Name; $pathchildCount+=1; ./yara64.exe -d filename=$_.Name TOOLKIT.yar $_.FullName 2> $path\$text }
+   $yarapathCount="Number of files scanned for " + $yarapath + " directory: " + $pathchildCount
+   $yarapathCount | Out-File "$path\$text" -Append
+}
 
 $childCount=0
 Get-ChildItem -Recurse -filter *.exe C:\ 2> $null |
 ForEach-Object { Write-Host -foregroundcolor "green" "Scanning"$_.FullName $_.Name; $childCount+=1; ./yara64.exe -d filename=$_.Name TOOLKIT.yar $_.FullName 2> $path\$text }
 
-$yarafileCount="Number of files scanned for C:\ directory: " + $childCount.Count
+$yarafileCount="Number of files scanned for C:\ directory: " + $childCount
 $yarafileCount | Out-File "$path\$text" -Append
 
 # run YARA on current processes
-#$processPath=Get-Process | Select-Object -Property Path
-#Get-ChildItem -Recurse -filter *.exe $processPath 2> $null |
-#ForEach-Object { Write-Host -foregroundcolor "green" "Scanning"$_.FullName $_.Name; $childCount+=1; ./yara64.exe -d filename=$_.Name TOOLKIT.yar $_.FullName 2> $path\$text }
-
+$processPath=Get-Process | Select-Object -Property Path
+Get-ChildItem -Recurse -filter *.exe $processPath 2> $null |
+ForEach-Object { Write-Host -foregroundcolor "green" "Scanning"$_.FullName $_.Name; $childCount+=1; ./yara64.exe -d filename=$_.Name TOOLKIT.yar $_.FullName 2> $path\$text }
 
 Write-Output "Beginning Processes Section..."
 
@@ -71,35 +70,6 @@ $processArray=Get-Process | Select-Object -Property Id, ProcessName, Path
 $procCount= "Number of current processes: " + $processArray.Count
 $current="CURRENT PROCESSES:"
 $current | Out-File "$path\$text" -Append
-
-$nullCount=0
-foreach ($proc in get-process)
-    {
-    try
-        {
-        $hashtable= Get-FileHash $proc.path -Algorithm SHA1 -ErrorAction stop
-        }
-    catch
-        {
-         #error handling... log contains names of processes where there was no path listed or we lack the rights
-         $nullCount+=1
-        }
-    }
-
-#foreach ( $row1 in $processArray ) {
-#  foreach ( $row2 in $hashtable ) {
-#    if ( $row1.ID -eq $row2.ID ) {
-  # BEGIN CALLOUT A
-#  $newtable= New-Object PSCustomObject -Property @{
-#    "ID" = $row1.ID
-#    "ProcessName" = $row1.ProcessName
-#    "SHA1 Hash" = $row2.Hash
-#    "Path" = $row1.Path
-#  } | Select-Object ID,ProcessName,Hash,Path
-  # END CALLOUT A
-#    }
-#  }
-#}
 
 $processArray | Out-File "$path\$text" -Append
 $procCount | Out-File "$path\$text" -Append
@@ -121,10 +91,11 @@ $stopCount | Out-File "$path\$text" -Append
 
 Write-Output "Reporting Process Finished..."
 
-$emailChoice= Read-Host -Prompt "Do you want to send report as email attachment? (Enter Y for Yes or N for No): "
+$emailChoice= Read-Host -Prompt "Do you want to send report as email attachment? (Enter Y for Yes or N for No) "
 if ($emailChoice -eq 'Y' -OR $emailChoice -eq 'y' -OR $emailChoice -eq 'yes' -OR $emailChoice -eq 'Yes'){
-     $userFrom=Read-Host -Prompt "Enter the email of the sender: "
-     $userTo=Read-Host -Prompt "Enter the email of the receiver in: "
-     Send-MailMessage -From "<$userFrom>" -To "<$userTo>" -Subject "Windows Artifact Report" -Body "Windows Artifact Report" -Attachments "$path\$text" -dno onSuccess, onFailure
+     $userFrom=Read-Host -Prompt "Enter the email of the sender "
+     $userTo=Read-Host -Prompt "Enter the email of the receiver in "
+     
+     Send-MailMessage -From "<$userFrom>" -To "<$userTo>" -Subject "Windows Artifact Report" -Body "Windows Artifact Report" -Attachments "$path\$text" -dno onSuccess, onFailure 
 }
 
