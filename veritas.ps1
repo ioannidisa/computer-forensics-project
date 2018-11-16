c102e0668d1638c69c822d3d8cd2515de9e1c075
ioannidisa <42744494+ioannidisa@users.noreply.github.com>
2018-11-16 12:43:08 -0500

    Update veritas.ps1

--- veritas.ps1
+++ veritas.ps1
@@ -123,11 +123,4 @@ $end | Out-File "$path\$text" -Append
 
 Write-Output "Reporting Process Finished..."
 
-$emailChoice= Read-Host -Prompt "Do you want to send report as email attachment? (Enter Y for Yes or N for No) "
-if ($emailChoice -eq 'Y' -OR $emailChoice -eq 'y' -OR $emailChoice -eq 'yes' -OR $emailChoice -eq 'Yes'){
-     $userFrom=Read-Host -Prompt "Enter the email of the sender "
-     $userTo=Read-Host -Prompt "Enter the email of the receiver in "
-     
-     Send-MailMessage -From "<$userFrom>" -To "<$userTo>" -Subject "Windows Artifact Report" -Body "Windows Artifact Report" -Attachments "$path\$text" -dno onSuccess, onFailure 
-}
 
